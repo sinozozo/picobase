06b04eb703717ef2ba3e14e850e567660dd902ab
zouqiang <410755274@qq.com>
2024-07-05 18:20:55 +0800

    文档补充

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -6,7 +6,7 @@ const pkg = require('vitepress/package.json')
 
 export const zh = defineConfig({
     lang: 'zh-Hans',
-    description: 'zzz由 Vite 和 Vue 驱动的静态站点生成器',
+    description: 'An open source low-code framework for building web applications.',
 
     themeConfig: {
         nav: nav(),
