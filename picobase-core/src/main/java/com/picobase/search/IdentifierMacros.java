a2e0ca7344271a99f8f1212d661657bce3c376a5
zouqiang <410755274@qq.com>
2024-07-18 10:27:34 +0800

    fix 宏时间获取错误

--- picobase-core/src/main/java/com/picobase/search/IdentifierMacros.java
+++ picobase-core/src/main/java/com/picobase/search/IdentifierMacros.java
@@ -7,41 +7,46 @@ import java.util.Map;
 
 public class IdentifierMacros {
 
-    private static final LocalDateTime now = LocalDateTime.now();
 
     private static final Map<String, Macro> macros = new HashMap<>();
 
     static {
-        macros.put("@now", () -> now);
-        macros.put("@second", () -> now.get(ChronoField.SECOND_OF_MINUTE));
-        macros.put("@minute", () -> now.get(ChronoField.MINUTE_OF_HOUR));
-        macros.put("@hour", () -> now.get(ChronoField.HOUR_OF_DAY));
-        macros.put("@day", () -> now.get(ChronoField.DAY_OF_MONTH));
-        macros.put("@month", () -> now.get(ChronoField.MONTH_OF_YEAR));
-        macros.put("@weekday", () -> now.get(ChronoField.DAY_OF_WEEK));
-        macros.put("@year", () -> now.get(ChronoField.YEAR));
+        macros.put("@now", () -> LocalDateTime.now());
+        macros.put("@second", () -> LocalDateTime.now().get(ChronoField.SECOND_OF_MINUTE));
+        macros.put("@minute", () -> LocalDateTime.now().get(ChronoField.MINUTE_OF_HOUR));
+        macros.put("@hour", () -> LocalDateTime.now().get(ChronoField.HOUR_OF_DAY));
+        macros.put("@day", () -> LocalDateTime.now().get(ChronoField.DAY_OF_MONTH));
+        macros.put("@month", () -> LocalDateTime.now().get(ChronoField.MONTH_OF_YEAR));
+        macros.put("@weekday", () -> LocalDateTime.now().get(ChronoField.DAY_OF_WEEK));
+        macros.put("@year", () -> LocalDateTime.now().get(ChronoField.YEAR));
         macros.put("@todayStart", () -> {
+            LocalDateTime now = LocalDateTime.now();
             LocalDateTime start = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), 0, 0, 0);
             return start;
         });
         macros.put("@todayEnd", () -> {
+            LocalDateTime now = LocalDateTime.now();
             LocalDateTime end = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), 23, 59, 59, 999999999);
             return end;
         });
         macros.put("@monthStart", () -> {
+            LocalDateTime now = LocalDateTime.now();
             LocalDateTime start = LocalDateTime.of(now.getYear(), now.getMonth(), 1, 0, 0, 0);
             return start;
         });
         macros.put("@monthEnd", () -> {
+            LocalDateTime now = LocalDateTime.now();
             LocalDateTime end = LocalDateTime.of(now.getYear(), now.getMonth(), 1, 23, 59, 59, 999999999);
             end = end.plusMonths(1).minusDays(1);
             return end;
         });
         macros.put("@yearStart", () -> {
+            LocalDateTime now = LocalDateTime.now();
             LocalDateTime start = LocalDateTime.of(now.getYear(), 1, 1, 0, 0, 0);
             return start;
         });
         macros.put("@yearEnd", () -> {
+            LocalDateTime now = LocalDateTime.now();
             LocalDateTime end = LocalDateTime.of(now.getYear(), 12, 31, 23, 59, 59, 999999999);
             return end;
         });
